f04b5b25f95abc02b5b2636dc71df01ca3868810
minsol-orang <minsol030507@naver.com>
2026-08-07 03:02:07 +0900

    ✨ Feat: RoutineSection 홈 화면에 연결

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,15 @@ import Header from "../../components/home/Header";
 import UserInfo from "../../components/home/UserInfo";
 import CalendarView from "../../components/home/CalendarView";
 import PhaseGuideBanner from "../../components/home/PhaseGuideBanner";
-import { getPhaseForDate } from "../../utils/cyclePhase";
-import { MOCK_USER, MOCK_PERIOD_CYCLES, MOCK_SKIN_RECORDS, PHASE_GUIDE } from "../../mocks/homeMock";
+import RoutineSection from "../../components/home/RoutineSection";
+import { getPhaseForDate, PHASE_LABEL } from "../../utils/cyclePhase";
+import {
+  MOCK_USER,
+  MOCK_PERIOD_CYCLES,
+  MOCK_SKIN_RECORDS,
+  PHASE_GUIDE,
+  PHASE_ROUTINES,
+} from "../../mocks/homeMock";
 
 export default function Home() {
   const today = dayjs().format("YYYY-MM-DD");
@@ -17,6 +24,9 @@ export default function Home() {
       <UserInfo name={MOCK_USER.name} skinType={MOCK_USER.skinType} skinConcerns={MOCK_USER.skinConcerns} />
       <CalendarView periodCycles={MOCK_PERIOD_CYCLES} skinRecords={MOCK_SKIN_RECORDS} />
       {phaseGuide && <PhaseGuideBanner title={phaseGuide.title} description={phaseGuide.description} />}
+      {currentPhase && (
+        <RoutineSection phaseLabel={PHASE_LABEL[currentPhase]} routines={PHASE_ROUTINES[currentPhase]} />
+      )}
     </div>
   );
 }
