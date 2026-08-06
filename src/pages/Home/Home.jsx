0e27542bfeed12a7a5e487d0b7530fee78d5996b
minsol-orang <minsol030507@naver.com>
2026-08-06 18:33:30 +0900

    ✨ Feat: PhaseGuideBanner를 홈 화면에 연결

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,22 @@
+import dayjs from "dayjs";
 import Header from "../../components/home/Header";
 import UserInfo from "../../components/home/UserInfo";
 import CalendarView from "../../components/home/CalendarView";
-import { MOCK_USER, MOCK_PERIOD_CYCLES, MOCK_SKIN_RECORDS } from "../../mocks/homeMock";
+import PhaseGuideBanner from "../../components/home/PhaseGuideBanner";
+import { getPhaseForDate } from "../../utils/cyclePhase";
+import { MOCK_USER, MOCK_PERIOD_CYCLES, MOCK_SKIN_RECORDS, PHASE_GUIDE } from "../../mocks/homeMock";
 
 export default function Home() {
+  const today = dayjs().format("YYYY-MM-DD");
+  const currentPhase = getPhaseForDate(today, MOCK_PERIOD_CYCLES);
+  const phaseGuide = currentPhase ? PHASE_GUIDE[currentPhase] : null;
+
   return (
     <div>
       <Header />
       <UserInfo name={MOCK_USER.name} skinType={MOCK_USER.skinType} skinConcerns={MOCK_USER.skinConcerns} />
       <CalendarView periodCycles={MOCK_PERIOD_CYCLES} skinRecords={MOCK_SKIN_RECORDS} />
+      {phaseGuide && <PhaseGuideBanner title={phaseGuide.title} description={phaseGuide.description} />}
     </div>
   );
 }
