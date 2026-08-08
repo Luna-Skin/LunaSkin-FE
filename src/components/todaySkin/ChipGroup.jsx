0725a77764831126c22390fb06819596e7e19ea7
minsol-orang <minsol030507@naver.com>
2026-08-09 02:53:23 +0900

    ✨ Feat: 라벨+선택 칩 목록 묶음 컴포넌트 ChipGroup 구현

---
+++ src/components/todaySkin/ChipGroup.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import ToggleChip from "./ToggleChip";
+
+const Container = styled.div`
+  display: flex;
+  width: 354px;
+  padding: 16px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  border-bottom: 0.5px dashed #ede8f8;
+  box-sizing: border-box;
+`;
+
+const Title = styled.span`
+  color: #000;
+  font-family: "Pretendard Variable";
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+const ChipList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 8px;
+  width: 100%;
+`;
+
+export default function ChipGroup({ title, options, isSelected, onSelect }) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <ChipList>
+        {options.map((option) => (
+          <ToggleChip key={option} label={option} selected={isSelected(option)} onClick={() => onSelect(option)} />
+        ))}
+      </ChipList>
+    </Container>
+  );
+}
