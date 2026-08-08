004bd1e3d1db1fcfbd19dfad4b65838eef8b3264
minsol-orang <minsol030507@naver.com>
2026-08-08 20:42:47 +0900

    🚚 Rename: SaveButton을 common에서 todaySkin으로 이동 및 스타일 구현

--- src/pages/TodaySkin/TodaySkinForm.jsx
+++ src/pages/TodaySkin/TodaySkinForm.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/todaySkin/Header";
+import RecordForm from "../../components/todaySkin/RecordForm";
 
 export default function TodaySkinForm() {
   return (
     <div>
       <Header />
+      <RecordForm />
     </div>
   );
 }
